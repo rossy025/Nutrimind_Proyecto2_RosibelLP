625601002aea2261296fc9d13f7ffd3f1a4f02b3
rossy025 <rosibel.lopez@uned.cr>
2025-08-09 16:01:57 -0600

    CreacionProyectoBase

---
+++ Nutrimind_Proyecto2_RosibelLP/src/nutrimind_proyecto2_rosibellp/Nutrimind_Proyecto2_RosibelLP.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nutrimind_proyecto2_rosibellp;
+
+/**
+ *
+ * @author carri
+ */
+public class Nutrimind_Proyecto2_RosibelLP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
